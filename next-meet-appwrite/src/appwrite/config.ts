c5a90df4ca490da5db3198bb26dc73138e789393
vai7725 <vaibhav.prajapat.7725@gmail.com>
2023-10-03 10:26:02 +0530

    appwrite and context api setup done

---
+++ next-meet-appwrite/src/appwrite/config.ts
@@ -0,0 +1,72 @@
+import config from '@/config/config'
+import { Client, Account, ID } from 'appwrite'
+
+type CreateUserAccount = {
+  email: string
+  password: string
+  name: string
+}
+type LoginUserAccount = {
+  email: string
+  password: string
+}
+
+const appwriteClient = new Client()
+appwriteClient
+  .setEndpoint(config.appwriteUrl)
+  .setProject(config.appwriteProjectId)
+
+export const account = new Account(appwriteClient)
+
+export class AppwriteService {
+  // create a new record of user inside appwrite
+  async createuserAccount({ email, password, name }: CreateUserAccount) {
+    try {
+      const userAccount = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      )
+      if (!userAccount) {
+        return this.login({ email, password })
+      } else {
+        return userAccount
+      }
+    } catch (error) {
+      throw error
+    }
+  }
+  async login({ email, password }: LoginUserAccount) {
+    try {
+      return await account.createEmailSession(email, password)
+    } catch (error) {
+      throw error
+    }
+  }
+  async isLoggedIn(): Promise<boolean> {
+    try {
+      const data = await this.getCurrentUser()
+      return Boolean(data)
+    } catch (error) {}
+    return false
+  }
+  async getCurrentUser() {
+    try {
+      return account.get()
+    } catch (error) {
+      console.log('getcurrentuser error - ', error)
+    }
+  }
+  async logout() {
+    try {
+      return await account.deleteSession('current')
+    } catch (error) {
+      console.log('logout error - ', error)
+    }
+  }
+}
+
+const appwriteService = new AppwriteService()
+
+export default appwriteService
